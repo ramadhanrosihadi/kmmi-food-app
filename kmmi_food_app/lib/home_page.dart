2ad1b2e7e8a83ccff83d50cff7aa9d01ed1e7b5e
Ramadhan Rosihadi Perdana <ramadhanrp.developer@gmail.com>
2021-09-09 08:01:57 +0700

    update

--- kmmi_food_app/lib/home_page.dart
+++ kmmi_food_app/lib/home_page.dart
@@ -16,8 +16,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   Recipe recipe = Recipe();
   String keyword = "chicken";
-  String text = "";
   List<RecipeModel> recipeModels = [];
+  TextEditingController textEditingController = TextEditingController();
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -26,6 +27,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   void getData() async {
+    setState(() => isLoading = true);
     Response response = await ApiProvider.callApi(
       "https://api.edamam.com/api/recipes/v2",
       {
@@ -36,35 +38,68 @@ class _HomePageState extends State<HomePage> {
       },
     );
     if (response.data != null) {
-      var responseJson = jsonDecode(jsonEncode(response.data.toString()));
-      List<RecipeModel> results = RecipeModel.fromListDynamic(jsonDecode(responseJson['hits']));
+      var jsonResponse = jsonDecode(jsonEncode(response.data));
+      List<RecipeModel> newRecipeModels = RecipeModel.fromListDynamic(jsonResponse['hits']);
       setState(() {
-        recipeModels = results;
+        recipeModels = newRecipeModels;
       });
-      print("recipeModels.length : ${results.length}");
+      print("recipeModels.length : ${newRecipeModels.length}");
     }
+    setState(() => isLoading = false);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: TextField(),
-      ),
-      body: Container(
-        width: double.infinity,
-        child: GestureDetector(
-          onTap: getData,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Response API : '),
-              Text(text),
-            ],
-          ),
+        backgroundColor: Colors.white,
+        title: TextField(
+          controller: textEditingController,
+          onSubmitted: (value) {
+            keyword = value;
+            getData();
+          },
+          textInputAction: TextInputAction.search,
         ),
+      ),
+      body: Builder(
+        builder: (context) {
+          if (isLoading) {
+            return Center(
+              child: Text('Mohon tunggu..'),
+            );
+          } else if (recipeModels.isEmpty) {
+            return Center(child: Text('Pencarian tidak ditemukan'));
+          }
+          return Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: ListView.separated(
+              itemBuilder: (context, index) {
+                RecipeModel data = recipeModels[index];
+                return Row(
+                  children: [
+                    Image.network(
+                      data.recipe.image,
+                      height: 50,
+                      width: 50,
+                    ),
+                    Expanded(
+                      child: ListTile(
+                        title: Text(data.recipe.label),
+                        subtitle: Text("${data.recipe.totalTime} minutes"),
+                      ),
+                    ),
+                  ],
+                );
+              },
+              separatorBuilder: (context, index) {
+                return Divider();
+              },
+              itemCount: recipeModels.length,
+            ),
+          );
+        },
       ),
     );
   }
